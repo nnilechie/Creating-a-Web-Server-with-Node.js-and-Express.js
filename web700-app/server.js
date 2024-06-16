bbb377c0906183514f1a61d345b3af953f0b9ae0
Nicholas Ilechie <160823545+nnilechie@users.noreply.github.com>
2024-06-15 22:51:52 -0400

    updating server script

--- web700-app/server.js
+++ web700-app/server.js
@@ -4,7 +4,7 @@
 *  of this assignment has been copied manually or electronically from any other source 
 *  (including 3rd party web sites) or distributed to other students.
 * 
-*  Name: Nicholas Nwanua Ilechie Student ID: 153461223 Date: June 15, 2024
+*  Name: Nicholas Nwanua Ilechie Student ID: 153461223  Date: June 15, 2024
 *
 ********************************************************************************/ 
 
